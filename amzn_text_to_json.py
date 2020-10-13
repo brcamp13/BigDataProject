015fd70508a23a3539b736d46340db058ccec4a7
brcamp13 <brandon.camp.bell@outlook.com>
2020-10-12 22:31:34 -0700

    Add some more stuff

--- amzn_text_to_json.py
+++ amzn_text_to_json.py
@@ -4,24 +4,37 @@ import json
 def process_amzn_text_to_json():
     review_data = []
     with open("amazon-meta.txt") as f:
-        # Start at line with first ID: 
-        # Once you've found this line, next line is ASIN
-        # Next line is title
-        # Group
-        # Salesrank
-        # Similar
-        # Categories. If > 0, each following line will have some information
-        # Reviews. If > 0, each following line will have some information
-        # Newline
-        # Start of new entry
-        # Repeat everything above
-        for line in f:
-            # Do all of the pseudocode above
-            print("Putting stuff here to remove error below.")
-            print(line)
+        for i in xrange(7):
+            next(f)
+
+        for line in f: 
+            product_record = dict()
+            line_content = line.split()
+            
+            if len(len_content) == 0:
+                review_data.append(product_record)
+                continue
+            elif line_content[0] == "id:":
+                # Process id
+            elif line_content[0] == "ASIN:":
+                # Process asin
+            elif line_content[0] == "title:":
+                # Process title
+            elif line_content[0] == "group:":
+                # Process group
+            elif line_content[0] == "salesrank:":
+                # Process salesrank
+            elif line_content[0] == "similar:":
+                # Process similar
+            elif line_content[0] == "categories:":
+                # Process categories
+            elif line_content[0] == "reviews:":
+                # Process reviews
+
 
     review_data = {"review_data": review_data}
     return json.dumps(review_data)
+
 
 
 if __name__ == "__main__":
