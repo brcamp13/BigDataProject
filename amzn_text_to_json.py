852c039437d3a11278c48700dbc3f059c2c8c3be
brcamp13 <brandon.camp.bell@outlook.com>
2020-10-14 21:52:57 -0700

    Finished all of the easy cases, haven't tested anything or even ran the thing

--- amzn_text_to_json.py
+++ amzn_text_to_json.py
@@ -3,27 +3,30 @@ import json
 
 def process_amzn_text_to_json():
     review_data = []
+    product_record = dict()
+
     with open("amazon-meta.txt") as f:
         for i in xrange(7):
             next(f)
 
         for line in f: 
-            product_record = dict()
             line_content = line.split()
             
             if len(len_content) == 0:
                 review_data.append(product_record)
+                product_record.clear()
                 continue
             elif line_content[0] == "id:":
-                # Process id
+                product_record["id"] = line_content[1]
             elif line_content[0] == "ASIN:":
-                # Process asin
+                product_record["ASIN"] = int(line_content[1])
             elif line_content[0] == "title:":
-                # Process title
+                title = " ".join([line_content[i] for i in range(1, len(line_content))])
+                product_record["title"] = title
             elif line_content[0] == "group:":
-                # Process group
+                product_record["group"] = line_content[1]
             elif line_content[0] == "salesrank:":
-                # Process salesrank
+                product_record["salesrank"] = int(line_content[1])
             elif line_content[0] == "similar:":
                 # Process similar
             elif line_content[0] == "categories:":
